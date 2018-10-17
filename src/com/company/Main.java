fba5c413711f09aeb5853796cbdaf3d2055cb54b
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-17 17:06:57 +0200

    Ejercicio 10

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int validarEntero(Scanner sc) {
+        int numeroValidado;
+        while (!sc.hasNextInt()) {
+            System.out.print("¡Introduce un valor numérico entero!");
+            sc.next();
+        }
+        numeroValidado = sc.nextInt();
+        return numeroValidado;
+    }
+
+    public static boolean validarNumero(int numero) {
+        boolean validado = true;
+        if (numero > 9999 || numero < 0) {
+            validado = false;
+        }
+        return validado;
+    }
+
+    public static int cuentaCifras(int numero) {
+        return String.valueOf(numero).length();
+    }
+
+    public static void main(String[] args) {
+        int numero, numeroCifras;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("¡Vamos a contar las cifras que tiene un número!");
+        System.out.print("Introduce un número: ");
+        numero = validarEntero(sc);
+        while (!validarNumero(numero)) {
+            System.out.print("¡Introduce un número entre 0 y 9999! ");
+            numero = validarEntero(sc);
+        }
+        numeroCifras = cuentaCifras(numero);
+        System.out.printf("EL número %d tiene %d cifras\n", numero, numeroCifras);
+    }
+}
